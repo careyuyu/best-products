9507e36984fe9b87a6804c97eb578b7d89200fa7
Hongwei Lou <careyuyu@hotmail.com>
2022-06-05 23:19:53 -0700

    init react frontend code

--- best-products-backend/src/main/java/com/careyuyu/bestproducts/bestproductsbackend/service/RabbitmqService.java
+++
@@ -1,8 +0,0 @@
-package com.careyuyu.bestproducts.bestproductsbackend.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface RabbitmqService {
-    public void sendMessage(String message);
-}
